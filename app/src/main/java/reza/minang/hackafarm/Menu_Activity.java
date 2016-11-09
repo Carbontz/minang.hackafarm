4f9acb8d35b37417a1fbefcb351bebf8b8fd892c
Carbontz <reza.blaze26@gmail.com>
2016-11-10 01:09:40 +0700

    aa

--- app/src/main/java/reza/minang/hackafarm/Menu_Activity.java
+++ app/src/main/java/reza/minang/hackafarm/Menu_Activity.java
@@ -1,7 +1,12 @@
 package reza.minang.hackafarm;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 public class Menu_Activity extends AppCompatActivity {
 
@@ -9,5 +14,17 @@ public class Menu_Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        TextView tvSelamatDatang = (TextView) findViewById(R.id.tvWelcome);
+        TextView tvUser = (TextView) findViewById(R.id.tvUser);
+        Spinner spListLadang = (Spinner) findViewById(R.id.spListLadang);
+        Button btnCheck = (Button) findViewById(R.id.btnCheck);
+
+        btnCheck.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(getApplicationContext(),Ladang_Activity.class));
+            }
+        });
     }
 }
